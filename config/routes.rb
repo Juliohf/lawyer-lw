bd4c7fff1166a1497ab6de97477fb9295c0856bb
Julio Franca <juliohcf@hotmail.com>
2022-09-01 14:13:29 -0300

    chat

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :users, only: [:index, :show]
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
 
   patch "restaurants/:id", to: "restaurants#update"
 
