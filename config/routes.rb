f3f2f8a42a34111ac814e7c2bf7e84901c74e199
Julio Franca <juliohcf@hotmail.com>
2022-09-01 15:12:27 -0300

    ngrok

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
 
   patch "restaurants/:id", to: "restaurants#update"
 
- master
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
