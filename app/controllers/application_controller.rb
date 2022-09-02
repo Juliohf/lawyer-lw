2ff1c2fa3b3e56ec86447e5067b3571d59a64593
Julio Franca <juliohcf@hotmail.com>
2022-09-02 11:25:12 -0300

    cloudinary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :user_type, :phone, :cpf, :oab, :address, :email, :description])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :user_type, :phone, :cpf, :oab, :address, :email, :description, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :user_type, :phone, :cpf, :oab, :address, :email, :description])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :user_type, :phone, :cpf, :oab, :address, :email, :description, :photo])
   end
 
   include Pundit::Authorization
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :messages
   has_many :reviews
+  has_one_attached :photo
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
