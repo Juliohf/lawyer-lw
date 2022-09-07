5d49c804760e7c57bd3e5cc6c9f9106b5e0ca39e
Julio Franca <juliohcf@hotmail.com>
2022-09-07 19:08:35 -0300

    a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
 
-    @category = ResponsesApi::RetrieveResponsesRequest.execute('HAd1hkqJ', token: 'tfp_8A68Ra49mjtGPARkHQhjNzpJ4ajyvVZfdgykcwcYPiXJ_3ssniHppZwqP9k').responses[0].answers[1].text
-    @location = ResponsesApi::RetrieveResponsesRequest.execute('HAd1hkqJ', token: 'tfp_8A68Ra49mjtGPARkHQhjNzpJ4ajyvVZfdgykcwcYPiXJ_3ssniHppZwqP9k').responses[0].answers[0].text
-    @nocategory = "I'm not sure"
+    @category = ResponsesApi::RetrieveResponsesRequest.execute('HAd1hkqJ', token: 'tfp_5CUAim62DW8oE1FJWmi3Uj32R6d22TqhE5JetZthYzLQ_3mNZ3CjtrpP96s').responses[0].answers[1].text
+    @location = ResponsesApi::RetrieveResponsesRequest.execute('HAd1hkqJ', token: 'tfp_5CUAim62DW8oE1FJWmi3Uj32R6d22TqhE5JetZthYzLQ_3mNZ3CjtrpP96s').responses[0].answers[0].text
+    @nocategory = "I'm not sure..."
 
     if @category == @nocategory
-      @tag =  ResponsesApi::RetrieveResponsesRequest.execute('HAd1hkqJ', token: 'tfp_8A68Ra49mjtGPARkHQhjNzpJ4ajyvVZfdgykcwcYPiXJ_3ssniHppZwqP9k').responses[0].answers[2].choice.label
+      @tag =  ResponsesApi::RetrieveResponsesRequest.execute('HAd1hkqJ', token: 'tfp_5CUAim62DW8oE1FJWmi3Uj32R6d22TqhE5JetZthYzLQ_3mNZ3CjtrpP96s').responses[0].answers[2].choice.label
       if @location.empty?
         @lawyers = policy_scope(User.tag_search(@tag))
       else
@@ -19,6 +19,10 @@ class UsersController < ApplicationController
     else
       @lawyers_location = User.location_search(@location)
       @lawyers = policy_scope(@lawyers_location.category_search(@category))
+    end
+    # @lawyers = policy_scope(User)
+    @markers = @lawyers.geocoded.map do |user|
+      { lat: user.latitude, lng: user.longitude }
     end
   end
 
