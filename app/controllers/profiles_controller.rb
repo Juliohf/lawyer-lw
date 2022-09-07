c3452cdb0302bc29775ae21ff21b4c6dda93daf4
Rachel Werneck <rachelwerneck@gmail.com>
2022-09-07 17:28:39 -0300

    profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,7 @@ class ProfilesController < ApplicationController
     @user = current_user
     authorize @user
     @review = Review.new
+    @chatrooms_as_client = current_user.chatrooms_as_client
+    @chatrooms_as_lawyer = current_user.chatrooms_as_lawyer
   end
 end
