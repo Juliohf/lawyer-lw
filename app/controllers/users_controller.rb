803c03b325ec14f3312573236c280fcbaa7302d2
camila lara <camila.alveslara@gmail.com>
2022-09-07 20:56:02 -0300

    popup maps, index stimulus and some fontend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,23 +2,28 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
 
-    @category = ResponsesApi::RetrieveResponsesRequest.execute('HAd1hkqJ', token: 'tfp_BXFnpWroeziNSAgyePWV2BRnp62MD6CyDiNZC1dsKLVh_3pdYTMpLQaLvTy').responses[0].answers[1].text
-    @location = ResponsesApi::RetrieveResponsesRequest.execute('HAd1hkqJ', token: 'tfp_BXFnpWroeziNSAgyePWV2BRnp62MD6CyDiNZC1dsKLVh_3pdYTMpLQaLvTy').responses[0].answers[0].text
-    @nocategory = "I'm not sure"
+    # @category = ResponsesApi::RetrieveResponsesRequest.execute('HAd1hkqJ', token: 'tfp_BXFnpWroeziNSAgyePWV2BRnp62MD6CyDiNZC1dsKLVh_3pdYTMpLQaLvTy').responses[0].answers[1].text
+    # @location = ResponsesApi::RetrieveResponsesRequest.execute('HAd1hkqJ', token: 'tfp_BXFnpWroeziNSAgyePWV2BRnp62MD6CyDiNZC1dsKLVh_3pdYTMpLQaLvTy').responses[0].answers[0].text
+    # @nocategory = "I'm not sure"
 
-    if @category == @nocategory
-      @tag =  ResponsesApi::RetrieveResponsesRequest.execute('HAd1hkqJ', token: 'tfp_BXFnpWroeziNSAgyePWV2BRnp62MD6CyDiNZC1dsKLVh_3pdYTMpLQaLvTy').responses[0].answers[2].choice.label
-      if @location.empty?
-        @lawyers = policy_scope(User.tag_search(@tag))
-      else
-        @lawyers_location = User.location_search(@location)
-        @lawyers = policy_scope(@lawyers_location.tag_search(@tag))
-      end
-    elsif @location.empty?
-      @lawyers = policy_scope(User.category_search(@category))
-    else
-      @lawyers_location = User.location_search(@location)
-      @lawyers = policy_scope(@lawyers_location.category_search(@category))
+    # if @category == @nocategory
+    #   @tag =  ResponsesApi::RetrieveResponsesRequest.execute('HAd1hkqJ', token: 'tfp_BXFnpWroeziNSAgyePWV2BRnp62MD6CyDiNZC1dsKLVh_3pdYTMpLQaLvTy').responses[0].answers[2].choice.label
+    #   if @location.empty?
+    #     @lawyers = policy_scope(User.tag_search(@tag))
+    #   else
+    #     @lawyers_location = User.location_search(@location)
+    #     @lawyers = policy_scope(@lawyers_location.tag_search(@tag))
+    #   end
+    # elsif @location.empty?
+    #   @lawyers = policy_scope(User.category_search(@category))
+    # else
+    #   @lawyers_location = User.location_search(@location)
+    #   @lawyers = policy_scope(@lawyers_location.category_search(@category))
+    # end(
+    @lawyers = policy_scope(User)
+
+    @markers = @lawyers.geocoded.map do |user|
+      { lat: user.latitude, lng: user.longitude, info_window: render_to_string(partial: "popup", locals: {user: user})}
     end
   end
 
