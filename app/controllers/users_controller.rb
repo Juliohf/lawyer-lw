081f74132ad1c90b2cc446f69203890a7e1dc377
camila lara <camila.alveslara@gmail.com>
2022-09-12 18:02:16 -0300

    lixo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,20 +6,21 @@ class UsersController < ApplicationController
     @location = ResponsesApi::RetrieveResponsesRequest.execute('HAd1hkqJ', token: ENV["TYPEFORM_API_TOKEN"]).responses[0].answers[0].text
     @nocategory = "I'm not sure..."
 
-    if @category == @nocategory
-      @tag =  ResponsesApi::RetrieveResponsesRequest.execute('HAd1hkqJ', token: ENV["TYPEFORM_API_TOKEN"]).responses[0].answers[2].choice.label
-      if @location.empty?
-        @lawyers = policy_scope(User.tag_search(@tag))
-      else
-        @lawyers_location = User.location_search(@location)
-        @lawyers = policy_scope(@lawyers_location.tag_search(@tag))
-      end
-    elsif @location.empty?
-      @lawyers = policy_scope(User.category_search(@category))
-    else
-      @lawyers_location = User.location_search(@location)
-      @lawyers = policy_scope(@lawyers_location.category_search(@category))
-    end
+    # if @category == @nocategory
+    #   @tag =  ResponsesApi::RetrieveResponsesRequest.execute('HAd1hkqJ', token: ENV["TYPEFORM_API_TOKEN"]).responses[0].answers[2].choice.label
+    #   if @location.empty?
+    #     @lawyers = policy_scope(User.tag_search(@tag))
+    #   else
+    #     @lawyers_location = User.location_search(@location)
+    #     @lawyers = policy_scope(@lawyers_location.tag_search(@tag))
+    #   end
+    # elsif @location.empty?
+    #   @lawyers = policy_scope(User.category_search(@category))
+    # else
+    #   @lawyers_location = User.location_search(@location)
+    #   @lawyers = policy_scope(@lawyers_location.category_search(@category))
+    # end
+    @lawyers = policy_scope(User)
 
     @markers = @lawyers.geocoded.map do |user|
       { lat: user.latitude, lng: user.longitude, info_window: render_to_string(partial: "popup", locals: {user: user})}
